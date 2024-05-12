ab2d2f9ca966a256301c6c007dd20411ec214c73
monocvet <monocvet@mail.ru>
2024-05-13 00:47:45 +0300

    Initial commit

---
+++ src/main/java/ru/maxima/HelloController.java
@@ -0,0 +1,17 @@
+package ru.maxima;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+// localhost:8080
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello-world";
+    }
+
+
+}
